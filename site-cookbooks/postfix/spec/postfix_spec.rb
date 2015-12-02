56cb4945b6d8e892129949a6ce5753078a0d94f1
root <root@serverspec.local.infra.active-ssa.com>
2015-12-02 20:30:31 +0900

    kammy:environment add

--- site-cookbooks/postfix/spec/postfix_spec.rb
+++ site-cookbooks/postfix/spec/postfix_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 puts "cookbook postfix"
 
-describe command("vzctl exec #{property["openvz"]["ctid"]} 'rpm -q postfix'") do
-  its(:stdout) { should match 'postfix' }
+if property["environment"]["set"] == "vps"
+  ctid = property["openvz"]["ctid"]
+  describe command("vzctl exec #{ctid} 'rpm -q postfix'") do
+    its(:stdout) { should match 'postfix' }
+  end
+end
+
+if property["environment"]["set"] == "aws"
+  describe command("ls") do
+    its(:exit_status) { should eq 0 }
+  end
 end
