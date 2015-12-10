692bc0db0cd6609e565f044374ee16e88cf22d93
root <root@serverspec.local.infra.active-ssa.com>
2015-12-10 21:39:51 +0900

    sg

--- site-cookbooks/_base/spec/a_base_spec.rb
+++ site-cookbooks/_base/spec/a_base_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper' # Load spec_helper
 os #Load os family
 
 #Load custom rescue type
-require 'serverspec/type/ec2_metadata_ami_id'
+#require 'serverspec/type/ec2_metadata_public_ip'
 require 'serverspec/type/ec2_metadata'
--- site-cookbooks/b_aws/ec2metadata/spec/aws_spec.rb
+++ site-cookbooks/b_aws/ec2metadata/spec/aws_spec.rb
@@ -1,13 +1,18 @@
 if property[:environment][:set] == "aws"
 puts "Run Recipe ec2metadata"
 ###
-describe ec2_metadata_ami_id("#{property[:ec2][:ami_id]}") do
+describe ec2_metadata_public_ip do
   it { should be_exist }
 end
-
+p property
 describe ec2_metadata("security-groups") do
-  its(:stdout) { should match %!default! }
-  its(:stdout) { should match %!avss-archiver! }
+  its(:stdout) { should match %!#{property[:ec2][:sg]}! }
+  #its(:stdout) { should match %!default\navss-archiver! }
 end
+
+describe ec2_metadata("iam/security-credentials") do
+ its(:stdout) { should match %!av_archiver! }
+end
+
 ###
 end
